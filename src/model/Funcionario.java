383788fe520c6f82a8997f9d2c8779af2b57ea24
Luiz <Luiz@Luiz-PC>
2016-02-18 18:08:52 -0200

    implementação do método incluir e trava do menu, impossibilitando estouro do vetor.

---
+++ src/model/Funcionario.java
@@ -0,0 +1,87 @@
+
+package model;
+
+/**
+ *
+ * @author Luiz Fernando de Souza Matricula: 225272 OD - Prof.ª Silvia Brunini
+ * ADS - Polo Sorocaba
+ */
+public class Funcionario {
+    
+    private String nome;
+    private int matricula;
+    private double salario;
+    
+    public Funcionario(){
+        
+        this.nome="";
+        this.matricula = 0;
+        this.salario = 0;
+        
+    }
+
+    public Funcionario(String nome, int matricula, double salario) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome = " + nome + ", matricula= " + matricula 
+                + ", salario= " + salario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + this.matricula;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Funcionario other = (Funcionario) obj;
+        if (this.matricula != other.matricula) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+}
---
+++ src/view/ManipulaVetoresDeObjetos.java
@@ -0,0 +1,153 @@
+package view;
+
+import java.util.Scanner;
+import model.Funcionario;
+
+/**
+ *
+ * @author Luiz
+ */
+public class ManipulaVetoresDeObjetos {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        Scanner linha = new Scanner(System.in);
+
+        Funcionario[] vetorFuncionario = new Funcionario[3];
+
+        Funcionario funcionario = new Funcionario("", 0, 0);
+
+        int opcao = -1;
+        int matricula = 0;
+
+        do {
+            System.out.println("****MENU****\n"
+                    + "1 - Inserir Funcionário na proxima posição vazia.\n"
+                    + "2 - Procurar por um funcionário por matricula.\n"
+                    + "3 - Procurar por um funcionário por nome.\n"
+                    + "4 - Remover um funcionário por matricula.\n"
+                    + "5 - Alterar o salário de um funcionário por matrícula\n"
+                    + "6 - Mostrar todos os funcionários.\n"
+                    + "7 - Encontrar o maior salário.\n"
+                    + "8 - Finaliza Programa.\n");
+
+            opcao = teclado.nextInt();
+
+            switch (opcao) {
+                case 1:
+
+                    if (size(vetorFuncionario) != vetorFuncionario.length) {
+
+                        matricula = size(vetorFuncionario);
+                        System.out.println("Nome:");
+                        String nome = linha.next();
+                        System.out.println("Salario:");
+                        double salario = teclado.nextDouble();
+
+                        funcionario = new Funcionario(nome, matricula, salario);
+
+                        inserir(vetorFuncionario, funcionario, matricula);
+
+                        mostrar(vetorFuncionario);
+
+                    } else {
+
+                        System.out.println("\n****Não existe posição"
+                                + " vazia na lista****\n");
+
+                    }
+
+                    break;
+                case 2:
+
+                    System.out.println(size(vetorFuncionario));
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+
+                    mostrar(vetorFuncionario);
+
+                    break;
+                case 7:
+                    break;
+                case 8:
+
+                    System.out.println("\nFim do Programa!");
+
+                    break;
+                default:
+                    System.out.println("\nOpção Inválida.");
+
+            }
+            System.out.println();
+        } while (opcao != 8);
+
+        teclado.close();
+        linha.close();
+
+    }
+
+    public static int size(Funcionario[] funcVetor) {
+
+        int cont = 0;
+
+        while (cont < funcVetor.length && funcVetor[cont] != null) {
+
+            cont++;
+
+        }
+
+        return cont;
+    }
+
+    public static void inserir(Funcionario[] vetFunc, Funcionario f, int posicao) {
+
+        if (size(vetFunc) < vetFunc.length) {
+
+            if (posicao < size(vetFunc)) {
+
+                for (int i = size(vetFunc); i < posicao; i++) {
+
+                    vetFunc[i] = vetFunc[i - 1];
+
+                }
+
+            } else if (posicao > size(vetFunc)) {
+
+                posicao = size(vetFunc);
+
+            }
+            vetFunc[posicao] = f;
+
+        }
+    }
+
+    public static void inserir(Funcionario[] vetorFunc, Funcionario f) {
+
+        if (size(vetorFunc) < vetorFunc.length) {
+
+            vetorFunc[size(vetorFunc)] = f;
+
+        }
+
+    }
+
+    public static void mostrar(Funcionario[] vetorFunc) {
+
+        for (int i = 0; i < size(vetorFunc); i++) {
+
+            System.out.println("Funcionário " + vetorFunc[i]);
+
+        }
+
+    }
+
+}
